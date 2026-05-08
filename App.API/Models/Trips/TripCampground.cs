94f32d74d5235799909300085035eedb16d7ca72
Kyle Haines <hainesk@sheridancollege.ca>
2026-05-08 10:09:05 -0400

    Added composite PK of TripId and CampgroundId

--- App.API/Models/Trips/TripCampground.cs
+++ App.API/Models/Trips/TripCampground.cs
@@ -1,6 +1,11 @@
-﻿namespace App.API.Models.Trips
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App.API.Models.Trips
 {
+    [PrimaryKey(nameof(TripId), nameof(CampgroundId))]
     public class TripCampground
     {
+        public int TripId { get; set; }
+        public int CampgroundId { get; set; }
     }
 }
