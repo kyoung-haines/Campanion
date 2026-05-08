c08355565412b28a1095c372d7c633fd9215eb45
Kyle Haines <hainesk@sheridancollege.ca>
2026-05-08 10:09:47 -0400

    Added middleware for Identity framework

--- App.API/Program.cs
+++ App.API/Program.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using App.API.Data;
+using App.API.Models;
+using App.API.Models.Identity;
 
 namespace App.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +18,35 @@ namespace App.API
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            // Retrieving and setting connection string
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<CampanionDbContext>(options => options.UseSqlServer(connectionString));
+
+            // Adding Identity related config
+            builder.Services.AddIdentity<AppUser, IdentityRole>()
+                .AddEntityFrameworkStores<CampanionDbContext>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+
+                using var scope = app.Services.CreateScope();
+
+                var dbContext = scope.ServiceProvider.GetRequiredService<CampanionDbContext>();
+                dbContext.Database.Migrate();
+
+                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                if (!await roleManager.RoleExistsAsync(Roles.Admin))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(Roles.Admin));
+                }
+                if (!await roleManager.RoleExistsAsync(Roles.Member))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(Roles.Member));
+                }
             }
 
             app.UseHttpsRedirection();
