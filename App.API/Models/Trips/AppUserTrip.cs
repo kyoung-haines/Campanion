3bfce1cdbadd45d5cb3d7596dd691be92500d8f9
Kyle Haines <hainesk@sheridancollege.ca>
2026-05-08 10:07:39 -0400

    Added class properties and composite PK from TripId and AppUserId

--- App.API/Models/Trips/AppUserTrip.cs
+++ App.API/Models/Trips/AppUserTrip.cs
@@ -1,6 +1,12 @@
-﻿namespace App.API.Models.Trips
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App.API.Models.Trips
 {
+    [PrimaryKey(nameof(TripId), nameof(AppUserId))]
     public class AppUserTrip
     {
+        public int TripId { get; set; }
+        public int AppUserId { get; set; }
+        public DateOnly AppUserTripeAddedAt { get; set; }
     }
 }
