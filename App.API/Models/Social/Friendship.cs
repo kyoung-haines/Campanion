272f481b1a3282f557bbacc7e4a7961a98fc88fc
kyoung-haines <k.younghaines@gmail.com>
2026-05-08 12:28:46 -0400

    Added class properties

--- App.API/Models/Social/Friendship.cs
+++ App.API/Models/Social/Friendship.cs
@@ -1,7 +1,11 @@
-﻿namespace App.API.Models.Social
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App.API.Models.Social
 {
+    [PrimaryKey(nameof(AppUserId1), nameof(AppUserId2))]
     public class Friendship
     {
-        public int FriendshipId { get; set; }
+        public int AppUserId1 { get; set; }
+        public int AppUserId2 { get; set; }
     }
 }
