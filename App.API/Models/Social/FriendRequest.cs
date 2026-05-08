bc05e32553c5db853668e2a74a5b2c15786ac558
Kyle Haines <hainesk@sheridancollege.ca>
2026-05-08 10:04:45 -0400

    Added class properties and composite PK

--- App.API/Models/Social/FriendRequest.cs
+++ App.API/Models/Social/FriendRequest.cs
@@ -1,6 +1,14 @@
-﻿namespace App.API.Models.Social
+﻿using App.API.Enums;
+using Microsoft.EntityFrameworkCore;
+namespace App.API.Models.Social
 {
+    [PrimaryKey(nameof(SenderAppUserId), nameof(ReceiverAppUserId))]
     public class FriendRequest
     {
+        public int SenderAppUserId { get; set; }
+        public int ReceiverAppUserId { get; set; }
+        public DateTime FriendRequestSentAt { get; set; }
+        public DateTime FriendRequestRespondedAt { get; set; }
+        public FriendRequestStatus FriendRequestStatus { get; set; }
     }
 }
