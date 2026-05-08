61c447945dd02773b0055990f86296378f870147
kyoung-haines <k.younghaines@gmail.com>
2026-05-08 12:28:35 -0400

    Added class properties

--- App.API/Models/Trips/Trip.cs
+++ App.API/Models/Trips/Trip.cs
@@ -1,7 +1,12 @@
-﻿namespace App.API.Models.Trips
+﻿using App.API.Enums;
+namespace App.API.Models.Trips
 {
     public class Trip
     {
         public int TripId { get; set; }
+        public TripStatus TripStatus { get; set; }
+        public DateTime TripStartDate { get; set; }
+        public DateTime TripEndDate { get; set; }
+        public DateTime TripCreationDate { get; set; }
     }
 }
