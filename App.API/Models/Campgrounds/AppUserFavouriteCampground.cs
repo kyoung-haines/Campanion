e99475ca02392ece1eeb03294d24fa80e766906f
Kyle Haines <hainesk@sheridancollege.ca>
2026-05-08 10:02:41 -0400

    Added composite PK from AppUserID and CampgroundId

--- App.API/Models/Campgrounds/AppUserFavouriteCampground.cs
+++ App.API/Models/Campgrounds/AppUserFavouriteCampground.cs
@@ -1,6 +1,12 @@
-﻿namespace App.API.Models.Campgrounds
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App.API.Models.Campgrounds
 {
+    [PrimaryKey(nameof(AppUserId), nameof(CampgroundId))]
     public class AppUserFavouriteCampground
     {
+        public int AppUserId { get; set; }
+        public int CampgroundId { get; set; }
+        public DateTime FavouritedAt { get; set; }
     }
 }
