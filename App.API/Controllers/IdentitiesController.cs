23ece97e641f71401ce64cf08155e1daeceb322d
kyoung-haines <k.younghaines@gmail.com>
2026-05-11 15:36:37 -0400

    Added packages and test endpoint

--- App.API/Controllers/IdentitiesController.cs
+++ App.API/Controllers/IdentitiesController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using App.API.Data;
+using App.API.Models.Identity;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace App.API.Controllers
 {
@@ -8,6 +11,18 @@ namespace App.API.Controllers
     [ApiController]
     public class IdentitiesController : ControllerBase
     {
-       
+        private readonly CampanionDbContext _context;
+        public IdentitiesController(CampanionDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetAllAppUsers()
+        {
+            var allAppUsers = await _context.AppUsers.ToListAsync();
+
+            return allAppUsers;
+        }
     }
 }
