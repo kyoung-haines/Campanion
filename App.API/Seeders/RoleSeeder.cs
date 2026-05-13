50fa993412cf202cd1747d646520440d71cc8b6d
kyoung-haines <k.younghaines@gmail.com>
2026-05-13 16:12:48 -0400

    Added class to handle seeding role data for development

---
+++ App.API/Seeders/RoleSeeder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using App.API.Services;
+namespace App.API.Seeders
+{
+    public class RoleSeeder
+    {
+        private readonly RolesService _roleService;
+
+        public RoleSeeder(RolesService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        public async Task SeedRolesAsync(RoleManager<IdentityRole> roleManager)
+        {
+            List<string> roles = await _roleService.GetAllRolesAsync();
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
