434786bfe5573aa7924d00372011385550dff73b
kyoung-haines <k.younghaines@gmail.com>
2026-05-14 13:12:32 -0400

    Added skeleton repository layer

---
+++ App.API/Repositories/Identity/AppUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using App.API.Data;
+using App.API.Models.Identity;
+
+namespace App.API.Repositories.Identity
+{
+    public class AppUserRepository
+    {
+        private readonly CampanionDbContext _context;
+
+        public AppUserRepository(CampanionDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task CreateAppUserAsync(AppUser appUser);
+        public Task DeleteAppUser(int id);
+        public Task<List<AppUser>> GetAllAppUserAsync()
+        {
+
+        }
+        public Task<AppUser> GetAppUserByIdAsync();
+        public Task UpdateAppUserAsync(int id);
+    }
+}
