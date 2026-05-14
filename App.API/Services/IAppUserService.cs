a8393d6ec64c19431d5416c8b657723649d977c4
kyoung-haines <k.younghaines@gmail.com>
2026-05-14 13:29:56 -0400

    Added parameter to CreateAppUserAsync method

---
+++ App.API/Services/IAppUserService.cs
@@ -0,0 +1,10 @@
+﻿using App.API.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace App.API.Services
+{
+    public interface IAppUserService
+    {
+        Task CreateAppUserAsync(UserManager<AppUser> user);
+    }
+}
