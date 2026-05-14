f7ec08a9731535c9707a0b308bd5a67bd31a2ef1
kyoung-haines <k.younghaines@gmail.com>
2026-05-14 12:53:03 -0400

    Added migration for modifying AppUser and Profile classes

--- App.API/Migrations/20260514164951_NullableProfileTesting.Designer.cs
+++ App.API/Migrations/20260514164951_NullableProfileTesting.Designer.cs
@@ -4,6 +4,7 @@ using App.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace App.API.Migrations
 {
     [DbContext(typeof(CampanionDbContext))]
-    partial class CampanionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260514164951_NullableProfileTesting")]
+    partial class NullableProfileTesting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ App.API/Migrations/20260514164951_NullableProfileTesting.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableProfileTesting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Profiles_AppUserProfileId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AppUserProfileId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserProfileId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AppUserProfileId",
+                table: "AspNetUsers",
+                column: "AppUserProfileId",
+                unique: true,
+                filter: "[AppUserProfileId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Profiles_AppUserProfileId",
+                table: "AspNetUsers",
+                column: "AppUserProfileId",
+                principalTable: "Profiles",
+                principalColumn: "ProfileId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Profiles_AppUserProfileId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_AppUserProfileId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AppUserProfileId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_AppUserProfileId",
+                table: "AspNetUsers",
+                column: "AppUserProfileId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Profiles_AppUserProfileId",
+                table: "AspNetUsers",
+                column: "AppUserProfileId",
+                principalTable: "Profiles",
+                principalColumn: "ProfileId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
