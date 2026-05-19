37380d9a3c8615ba8c09b850893184316d0a8032
kyoung-haines <k.younghaines@gmail.com>
2026-05-19 08:19:30 -0400

    Added skeleton CampgroundRepository layer class

---
+++ App.API/Repositories/CampgroundRepository.cs
@@ -0,0 +1,41 @@
+﻿using App.API.Models.Campgrounds;
+using App.API.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace App.API.Repositories
+{
+    public class CampgroundRepository
+    {
+        private readonly ILogger<CampgroundRepository> _logger;
+        private readonly CampanionDbContext _context;
+
+        public CampgroundRepository(ILogger<CampgroundRepository> logger, CampanionDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        async Task DeleteCampground(int id)
+        {
+            _logger.LogInformation($"Attempting to delete user with ID: {id}...");
+            _logger.LogInformation($"Looking for campground in the system...");
+
+            var campground = await _context.FindAsync<Campground>(id);
+
+            if(campground == null)
+            {
+                _logger.LogError("Campground not found. Check the ID value.");
+                throw new Exception("Campground not found. Please try again.");
+            }
+
+            _logger.LogInformation("Campground found. Attempting to delete...");
+
+            _context.Remove<Campground>(campground);
+
+            
+        }
+        Task<IEnumerable<Campground>> GetAllCampgroundsAsync();
+        Task<Campground> GetCampgroundByIdAsync(int id);
+        Task UpdateCampgroundAsync(int id);
+    }
+}
