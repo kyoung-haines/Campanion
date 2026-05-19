3c81209b7feaec8096fa6a2e5e4bdae968daf27d
kyoung-haines <k.younghaines@gmail.com>
2026-05-19 10:29:18 -0400

    Formatting

--- App.API/Services/ICampgroundService.cs
+++ App.API/Services/ICampgroundService.cs
@@ -4,13 +4,13 @@ namespace App.API.Services
 {
     public interface ICampgroundService
     {
-        public Task<bool> CampgroundIdIsExistsAsync(int newCampId);
-        public Task<bool> IsCampgroundUpdatedAsync(Campground originalCampground, Campground updatedCampground);
         public Task DeleteCampground(int id);
         public Task<IEnumerable<Campground>> GetAllCampgroundsAsync();
         public Task<Campground> GetCampgroundByIdAsync(int id);
         public Task UpdateCampgroundAsync(int id);
         public Task AddCampgroundAsync(Campground campground);
+        public Task<bool> CampgroundIdIsExistsAsync(int newCampId);
+        public Task<bool> IsCampgroundUpdatedAsync(Campground originalCampground, Campground updatedCampground);
         public Task<bool> IsCampgroundAddedAsync(int newCampgroundId);
     }
 }
