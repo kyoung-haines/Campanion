b98681b99fb9491395a416132a0dfc01af4b2955
kyoung-haines <k.younghaines@gmail.com>
2026-05-19 08:23:11 -0400

    Added skeleton CampgroundService class

---
+++ App.API/Services/CampgroundService.cs
@@ -0,0 +1,13 @@
+﻿using App.API.Models.Campgrounds;
+
+namespace App.API.Services
+{
+    public interface CampgroundService
+    {
+
+        Task<bool> CampgroundIsAdded(IEnumerable<Campground> campgrounds)
+        {
+
+        }
+    }
+}
