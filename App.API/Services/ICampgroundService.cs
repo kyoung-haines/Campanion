d27edb0ac54c5600cfca86660eeceb573d3c63b5
kyoung-haines <k.younghaines@gmail.com>
2026-05-19 10:25:26 -0400

    Added method signatures

--- App.API/Services/ICampgroundService.cs
+++ App.API/Services/ICampgroundService.cs
@@ -4,6 +4,13 @@ namespace App.API.Services
 {
     public interface ICampgroundService
     {
-        Task<bool> CampgroundIsAdded(IEnumerable<Campground> campgrounds);
+        public Task<bool> CampgroundIdIsExistsAsync(int newCampId);
+        public Task<bool> IsCampgroundUpdatedAsync(Campground originalCampground, Campground updatedCampground);
+        public Task DeleteCampground(int id);
+        public Task<IEnumerable<Campground>> GetAllCampgroundsAsync();
+        public Task<Campground> GetCampgroundByIdAsync(int id);
+        public Task UpdateCampgroundAsync(int id);
+        public Task AddCampgroundAsync(Campground campground);
+        public Task<bool> IsCampgroundAddedAsync(int newCampgroundId);
     }
 }
