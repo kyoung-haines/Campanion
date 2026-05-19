4fee44e33cefea9d91df19b830b283ef2f5cdb5a
kyoung-haines <k.younghaines@gmail.com>
2026-05-19 08:18:48 -0400

    Added interface for Campground repository layer

---
+++ App.API/Repositories/ICampgroundRepository.cs
@@ -0,0 +1,12 @@
+﻿using App.API.Models.Campgrounds;
+
+namespace App.API.Repositories
+{
+    public interface ICampgroundRepository
+    {
+        public Task<IEnumerable<Campground>> GetAllCampgroundsAsync();
+        public Task<Campground> GetCampgroundByIdAsync(int id);
+        public Task UpdateCampgroundAsync(int id);
+        public Task DeleteCampground(int id);
+    }
+}
