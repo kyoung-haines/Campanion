cfa47101287297205c9ef3d17004db5ab3d0c1f5
kyoung-haines <k.younghaines@gmail.com>
2026-05-19 08:22:58 -0400

    Added Interface for Campground Service layer

---
+++ App.API/Services/ICampgroundService.cs
@@ -0,0 +1,9 @@
+﻿using App.API.Models.Campgrounds;
+
+namespace App.API.Services
+{
+    public interface ICampgroundService
+    {
+        Task<bool> CampgroundIsAdded(IEnumerable<Campground> campgrounds);
+    }
+}
