c62c33a9b430c2dd66681fa9fe13bb8109d55c50
kyoung-haines <k.younghaines@gmail.com>
2026-05-20 10:36:26 -0400

    Added ResultPattern class for operation outcome feedback

---
+++ App.API/Result.cs
@@ -0,0 +1,15 @@
+﻿namespace App.API
+{
+    public class Result<T>
+    {
+        public bool Succeeded { get; set;}
+        public T? Data { get; set;}
+        public string? Error { get; set;}
+
+        public static Result<T> Success(T data) =>
+            new() { Succeeded = true, Data = data };
+
+        public static Result<T> Failure(string error) =>
+            new() { Succeeded = true, Error = error };
+    }
+}
