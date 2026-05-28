1e084499f5600ba2f4f3891ec9e7ac875700eb66
kyoung-haines <k.younghaines@gmail.com>
2026-05-28 13:32:27 -0400

    Added GetAllFavouriteCampgroundsAsync method

--- App.API/Repositories/AppUserFavouriteCampgroundRepository.cs
+++ App.API/Repositories/AppUserFavouriteCampgroundRepository.cs
@@ -1,5 +1,6 @@
 ﻿using App.API.Data;
 using App.API.Models.Campgrounds;
+using App.API.Exceptions.AppUserExceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace App.API.Repositories
@@ -33,18 +34,40 @@ namespace App.API.Repositories
                 return Result<bool>.Failure($"Failed to delete the favourite campground.");
             }
         }
-        public async Task<Result<List<AppUserFavouriteCampground>>> GetAllFavouriteCampgroundsAsync()
+        public async Task<Result<List<AppUserFavouriteCampground>>> GetAllFavouriteCampgroundsAsync(int appUserId)
         {
             try
             {
-                _logger.LogInformation($"Attempting to retrieve all favourite campgrounds for current user...");
+                var favCampgrounds = new List<AppUserFavouriteCampground>();
 
-                var favCampgrounds = await _context.AppUserFavouriteCampgrounds.ToListAsync();
+                var appUser = await _context.AppUsers.FindAsync(appUserId);
+
+                if(appUser != null)
+                {
+                    _logger.LogInformation($"Attempting to retrieve all favourite campgrounds for current user...");
+
+                    favCampgrounds = await _context.AppUserFavouriteCampgrounds
+                        .Where(fav => fav.AppUserId == appUserId)
+                        .ToListAsync();
+
+
+                    if (favCampgrounds.Count() == 0)
+                    {
+                        _logger.LogWarning($"Favourites list is empty. User either has no favourites or an error occurred...");
+                    }
+                }
+                else
+                {
+                    _logger.LogError($"AppUserId: {appUserId} does not exist.");
+                    throw new AppUserException($"Invalid UserID. Verify that ID: {appUserId} exists.");
+                }
+
+                return Result<List<AppUserFavouriteCampground>>.Success(favCampgrounds);
             }
             catch (Exception ex)
             {
-
-                throw;
+                _logger.LogError(ex, $"Unable to retrieve user's favourite campgrounds. See exception for details.");
+                return Result<List<AppUserFavouriteCampground>>.Failure(ex.Message);
             }
         }
         public async Task<Result<AppUserFavouriteCampground>> GetFavouriteCampgroundByPrimaryKey(int campId, int userId)
