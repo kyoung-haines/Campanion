bea5c8e3228241548c852e1a4a3c548547faee9b
kyoung-haines <k.younghaines@gmail.com>
2026-05-28 13:33:42 -0400

    Modified GetAllFavouriteCampgroundsAsync method

--- App.API/Services/AppUserFavouriteCampgroundService.cs
+++ App.API/Services/AppUserFavouriteCampgroundService.cs
@@ -21,26 +21,26 @@ namespace App.API.Services
             try
             {
                 _logger.LogInformation($"Attempting to delete campground from favourites...");
-                
+
                 if(favCampground == null)
                 {
                     _logger.LogWarning($"Favourite Campground cannot be deleted. Object is null...");
                 }
 
                 var result = await _favCampgroundRepo.DeleteFavouriteCampgroundAsync(favCampground);
-                
+
                 if(result.Succeeded == true)
                 {
                     _logger.LogInformation($"Campground successfully removed from favourites...");
                 }
 
-                return Result<bool>.Success(true);
+                return result;
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, $"Error deleting campground from favourites...");
                 return Result<bool>.Failure("Failed to delete campground from favourites.");
-                
+
             }
         }
 
@@ -48,9 +48,27 @@ namespace App.API.Services
         {
             return Result<AppUserFavouriteCampground>.Failure("Testing...");
         }
-        public async Task<Result<List<AppUserFavouriteCampground>>> GetAllFavouriteCampgroundsAsync()
+        public async Task<Result<List<AppUserFavouriteCampground>>> GetAllFavouriteCampgroundsAsync(int appUserId)
         {
-            return Result<List<AppUserFavouriteCampground>>.Failure("Testing...");
+            try
+            {
+                _logger.LogInformation($"Attempting to retrieve current user's favourite campgrounds...");
+                var favCampgrounds = await _favCampgroundRepo.GetAllFavouriteCampgroundsAsync(appUserId);
+
+                if(favCampgrounds.Succeeded == true)
+                {
+                    _logger.LogInformation($"Successfully retrieved user's favourite campgrounds...");
+                }
+
+                return Result<List<AppUserFavouriteCampground>>.Success(favCampgrounds.Data);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Unable to retrieve user's favourite campgrounds. See exception for details...");
+
+                return Result<List<AppUserFavouriteCampground>>.Failure("Failed to retrieve user's favourite campgrounds.");
+            }            
         }
         public async Task<Result<AppUserFavouriteCampground>> GetFavouriteCampgroundByPrimaryKey(int campId, int userId)
         {
