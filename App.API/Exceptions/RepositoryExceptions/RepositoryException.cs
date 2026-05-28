1d8022a71de863826a5ec6724cd27d4ae5b0ee0f
kyoung-haines <k.younghaines@gmail.com>
2026-05-28 11:07:52 -0400

    Modified file structure for custom excepions

---
+++ App.API/Exceptions/RepositoryExceptions/RepositoryException.cs
@@ -0,0 +1,9 @@
+﻿namespace App.API.Exceptions.RepositoryExceptions
+{
+    public class RepositoryException : Exception
+    {
+        public RepositoryException() { }
+        public RepositoryException(string message) : base(message) { }
+        public RepositoryException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
