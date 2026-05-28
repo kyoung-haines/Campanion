4fcdfd305a4febade3842d442b0d09daff9e7716
kyoung-haines <k.younghaines@gmail.com>
2026-05-28 11:07:31 -0400

    Added base AppUserException class for custom AppUser exceptions

---
+++ App.API/Exceptions/AppUserExceptions/AppUserException.cs
@@ -0,0 +1,9 @@
+﻿namespace App.API.Exceptions.AppUserExceptions
+{
+    public class AppUserException : Exception
+    {
+        public AppUserException() { }
+        public AppUserException(string message) : base(message) { }
+        public AppUserException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
