10700aeb787132d6f7fbdd1e8eeecf00d3dc6115
kyoung-haines <k.younghaines@gmail.com>
2026-05-28 11:09:55 -0400

    Added custom exception for invalid AppUserId values

---
+++ App.API/Exceptions/AppUserExceptions/InvalidUserIdException.cs
@@ -0,0 +1,11 @@
+﻿namespace App.API.Exceptions.AppUserExceptions
+{
+    public class InvalidUserIdException : AppUserException
+    {
+        public InvalidUserIdException() { }
+        public InvalidUserIdException(string message) : base(message) { }
+        public InvalidUserIdException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
