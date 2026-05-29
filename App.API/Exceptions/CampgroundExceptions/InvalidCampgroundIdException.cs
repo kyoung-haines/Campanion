0770e6d3da9f7ed9827577e6731d0e5cf5b5ec54
kyoung-haines <k.younghaines@gmail.com>
2026-05-29 13:29:03 -0400

    Modified name to include Exception suffix

---
+++ App.API/Exceptions/CampgroundExceptions/InvalidCampgroundIdException.cs
@@ -0,0 +1,9 @@
+﻿namespace App.API.Exceptions.CampgroundExceptions
+{
+    public class InvalidCampgroundIdException : CampgroundException
+    {
+        public InvalidCampgroundIdException() { }
+        public InvalidCampgroundIdException(string message) : base(message) { }
+        public InvalidCampgroundIdException(string message, Exception innerException) : base(message, innerException) { } 
+    }
+}
