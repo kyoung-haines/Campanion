d91198dae51f22d8dc3bdee45855a60e42be3a5a
kyoung-haines <k.younghaines@gmail.com>
2026-05-29 13:21:02 -0400

    Added base exception class for custom Campground exceptions

---
+++ App.API/Exceptions/CampgroundExceptions/CampgroundException.cs
@@ -0,0 +1,9 @@
+﻿namespace App.API.Exceptions.CampgroundExceptions
+{
+    public class CampgroundException : Exception
+    {
+        public CampgroundException() { }
+        public CampgroundException(string message) : base(message) { }
+        public CampgroundException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
