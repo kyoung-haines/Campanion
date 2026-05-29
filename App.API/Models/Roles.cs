212baef1fffc6a307b0d26ca9ac739614a5ae567
kyoung-haines <k.younghaines@gmail.com>
2026-05-29 19:17:27 -0400

    Modified role names

--- App.API/Models/Roles.cs
+++ App.API/Models/Roles.cs
@@ -2,12 +2,12 @@
 {
     public static class Roles
     {
-        public const string Admin = "Admin";
-        public const string Member = "User";
+        public const string Admin = "ADMINISTRATOR";
+        public const string Member = "REGULAR_USER";
 
         public static List<string> AllRoles()
         {
-            var roles = new List<string> { "Admin", "User" };
+            var roles = new List<string> { "ADMINISTRATOR", "REGULAR_USER" };
             
             return roles;
         }
