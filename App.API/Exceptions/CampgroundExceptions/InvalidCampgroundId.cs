8131c114a05d9f03ac80c632f7884093885d7c0f
kyoung-haines <k.younghaines@gmail.com>
2026-05-29 13:21:22 -0400

    Added exception for invalid campground ID

---
+++ App.API/Exceptions/CampgroundExceptions/InvalidCampgroundId.cs
@@ -0,0 +1,9 @@
+﻿namespace App.API.Exceptions.CampgroundExceptions
+{
+    public class InvalidCampgroundId : CampgroundException
+    {
+        public InvalidCampgroundId() { }
+        public InvalidCampgroundId(string message) : base(message) { }
+        public InvalidCampgroundId(string message, Exception innerException) : base(message, innerException) { } 
+    }
+}
