6f4e13e806ca4a547e0d9916dbd51456a7c2dbfd
kyoung-haines <k.younghaines@gmail.com>
2026-05-29 13:46:09 -0400

    Implementing IAppUserRepository for AppUser repo layer

---
+++ App.API/Repositories/AppUserRepository.cs
@@ -0,0 +1,41 @@
+﻿using App.API.Data;
+using App.API.Models.Identity;
+using App.API.Services;
+using Microsoft.EntityFrameworkCore;
+namespace App.API.Repositories
+{
+    public class AppUserRepository : IAppUserRepository
+    {
+        private readonly ILogger<AppUserRepository> _logger;
+        private readonly CampanionDbContext _context;
+
+        public AppUserRepository(ILogger<AppUserRepository> logger, CampanionDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<Result<List<AppUser>>> GetAllAppUsersAsync()
+        {
+            try
+            {
+                _logger.LogInformation("AppUserRepository method called: GetAllAppUsers()...");
+                _logger.LogInformation("Attempting to retrieve all app users...");
+
+                var appUsers = await _context.AppUsers.ToListAsync();
+
+                if(appUsers.Count() <= 0)
+                {
+                    _logger.LogWarning("AppUsers list is empty. If there are registered users, an error has occurred...");
+                }
+
+                return Result<List<AppUser>>.Success(appUsers);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error retrieving users from the database...");
+                return Result<List<AppUser>>.Failure($"Failed to retrieve users from the database.");
+            }
+        }
+    }
+}
