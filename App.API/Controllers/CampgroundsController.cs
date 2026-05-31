ffc8ee1d3eeac9f24bee6c4c3cff0923c790a9e8
kyoung-haines <k.younghaines@gmail.com>
2026-05-30 20:01:44 -0400

    Added note for future self

--- App.API/Controllers/CampgroundsController.cs
+++ App.API/Controllers/CampgroundsController.cs
@@ -9,3 +9,11 @@ namespace App.API.Controllers
     {
     }
 }
+
+/*
+ * For future me:
+ * When I'm implementing the Controllers, and I come to this one, 
+ * I want to create a base controller class that will include a routing prefix template.
+ * That new class will inherit from ControllerBase, while this class will inherit from 
+ * new custom base controller.
+ */
