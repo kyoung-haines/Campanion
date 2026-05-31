2ba2bce385c8eed211d5e91e6e30d3e91f3ed50b
kyoung-haines <k.younghaines@gmail.com>
2026-05-30 20:18:00 -0400

    Added UpdateAppUserAsync method signature

--- App.API/Repositories/IAppUserRepository.cs
+++ App.API/Repositories/IAppUserRepository.cs
@@ -9,5 +9,6 @@ namespace App.API.Repositories
         public Task<Result<List<AppUser>>> GetAllRegularAppUsersAsync();
         public Task<Result<AppUser>> GetAppUserByIdAsync(int appUserId);
         public Task<Result<bool>> DeleteAppUserAsync(int appUserId);
+        public Task<Result<AppUser>> UpdateAppUserAsync(int appUserId);
     }
 }
