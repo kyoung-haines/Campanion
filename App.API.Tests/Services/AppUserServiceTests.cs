e6b850e47aba5425bd538151cbf176f08946da06
kyoung-haines <k.younghaines@gmail.com>
2026-06-04 11:36:38 -0400

    Added test class skeleton

---
+++ App.API.Tests/Services/AppUserServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.API.Repositories;
+using App.API.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace App.API.Tests.Services
+{
+    [TestClass]
+    public class AppUserServiceTests
+    {
+        private Mock<ILogger> _mockLogger;
+        private Mock<IAppUserRepository> _mockAppUserRepository;
+        private IAppUserService _userService;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _mockLogger = new Mock<ILogger>();
+            _mockAppUserRepository = new Mock<IAppUserRepository>();
+            _userService = new AppUserService(_mockLogger.Object, _mockAppUserRepository.Object);
+        }
+
+        [TestMethod]
+        public void GetAllAppUsersAsync()
+        {
+
+        }
+    }
+}
