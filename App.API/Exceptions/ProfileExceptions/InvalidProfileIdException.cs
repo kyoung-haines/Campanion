6bf5768bb73358e2a95281098589ddb1a75a66ac
kyoung-haines <k.younghaines@gmail.com>
2026-06-11 10:07:56 -0400

    Added custom exception for invalid ProfileId values

---
+++ App.API/Exceptions/ProfileExceptions/InvalidProfileIdException.cs
@@ -0,0 +1,16 @@
+﻿namespace App.API.Exceptions.ProfileExceptions
+{
+    public class InvalidProfileIdException : Exception
+    {
+        public InvalidProfileIdException(string message) : base(message)
+        {
+        }
+        public InvalidProfileIdException()
+        {
+        }
+        
+        public InvalidProfileIdException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
