ecff359f21e8da20de2f51e76f2c33582bbbf1b9
kyoung-haines <k.younghaines@gmail.com>
2026-06-11 09:49:32 -0400

    Added method signatures for repo layer

---
+++ App.API/Repositories/IProfileRepository.cs
@@ -0,0 +1,11 @@
+﻿using App.API.Models.Identity;
+
+namespace App.API.Repositories
+{
+    public interface IProfileRepository
+    {
+        public Task<Result<Profile>> GetProfileByIdAsync(int id);
+        public Task<Result<Profile>> UpdateProfileAsync(int id);
+        public Task<Result<bool>> DeleteProfileAsync(int id);
+    }
+}
