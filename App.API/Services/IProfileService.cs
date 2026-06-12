d105867779103d2b8498a31ac27dabe071b14087
kyoung-haines <k.younghaines@gmail.com>
2026-06-12 13:25:29 -0400

    Added method signatures

---
+++ App.API/Services/IProfileService.cs
@@ -0,0 +1,12 @@
+﻿using App.API.Models.Identity;
+
+namespace App.API.Services
+{
+    public interface IProfileService
+    {
+        public Task<Result<Profile>> GetProfileByIdAsync(int id);
+        public Task<Result<Profile>> UpdateProfileAsync(int id);
+        public Task<Result<bool>> DeleteProfileAsync(int id);
+        public Task<Result<Profile>> CreateNewProfileAsync(AppUser newUser);
+    }
+}
