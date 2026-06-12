618c77c917e5c42d5a31b39c3f1d8e31d267f3e6
kyoung-haines <k.younghaines@gmail.com>
2026-06-12 13:25:45 -0400

    Added skeleton service layer class

---
+++ App.API/Services/ProfileService.cs
@@ -0,0 +1,32 @@
+﻿using App.API.Models.Identity;
+using App.API.Repositories;
+
+namespace App.API.Services
+{
+    public class ProfileService
+    {
+		private ILogger<ProfileService> _logger;
+		private IProfileRepository _profileRepository;
+
+		public ProfileService(ILogger<ProfileService> logger, IProfileRepository profileRepository)
+		{
+			_logger = logger;
+			_profileRepository = profileRepository;
+        }
+
+        public async Task<Result<Profile>> GetProfileByIdAsync(int id)
+        {
+			try
+			{
+				_logger.LogInformation("ProfileService method called: GetProfileByIdAsync...");
+				var profileResult = await _profileRepository.GetProfileByIdAsync(id);
+				return profileResult;
+            }
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, $"Failed to get profile by ID: {id}...");
+				return Result<Profile>.Failure($"An error occurred while retrieving the profile: {ex.Message}");
+            }
+        }
+    }
+}
