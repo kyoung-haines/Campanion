928ceb43db8b6b319a44c32b2c5280a7189a6101
kyoung-haines <k.younghaines@gmail.com>
2026-06-17 17:31:52 -0400

    Added more of the service layer methods

--- App.API/Services/ProfileService.cs
+++ App.API/Services/ProfileService.cs
@@ -28,5 +28,57 @@ namespace App.API.Services
 				return Result<Profile>.Failure($"An error occurred while retrieving the profile: {ex.Message}");
             }
         }
+
+		public async Task<Result<Profile>> UpdateProfileAsync(int id)
+		{
+			try
+			{
+				_logger.LogInformation("ProfileService method called: UpdateProfileAsync...");
+
+				var profileResult = await _profileRepository.UpdateProfileAsync(id);
+
+				return Result<Profile>.Success(profileResult.Data);
+            }
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, $"Failed to update profile with ID: {id}...");
+				return Result<Profile>.Failure($"An error occurred while updating the profile: {ex.Message}");
+            }
+        }
+
+		public async Task<Result<bool>> DeleteProfileAsync(int id)
+		{
+			try
+			{
+                _logger.LogInformation("ProfileService method called: DeleteProfileAsync...");
+
+                var profileResult = await _profileRepository.DeleteProfileAsync(id);
+
+				return Result<bool>.Success(true);
+
+            }
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, $"Failed to delete profile with ID: {id}...");
+				return Result<bool>.Failure("An error occurred attempting to delete the profile. Try again");
+			}
+        }
+
+		public async Task<Result<Profile>> CreateNewProfileAsync(AppUser newUser)
+		{
+			try
+			{
+				_logger.LogInformation("ProfileService method called: CreateNewProfileAsync...");
+				var newUserResult = await _profileRepository.CreateNewProfileAsync(newUser);
+
+				return Result<Profile>.Success(newUserResult.Data);
+            }
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, $"Failed to create new profile in the system)");
+
+				return Result<Profile>.Failure($"Failed to create new profile in the system: {ex.Message}");
+            }
+		}
     }
 }
