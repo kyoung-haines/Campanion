c402026799cbc1bf89031fb2fa1ea20661bc5373
kyoung-haines <k.younghaines@gmail.com>
2026-06-18 11:41:44 -0400

    Added method signature for Trip repository layer

---
+++ App.API/Repositories/ITripRepository.cs
@@ -0,0 +1,14 @@
+﻿using App.API.Models.Trips;
+
+namespace App.API.Repositories
+{
+    public interface ITripRepository
+    {
+        Task<Result<List<Trip>>> GetAllTripsAsync();
+        Task<Result<List<Trip>>> GetTripsByUserIdAsync(int userId);
+        Task<Result<Trip>> UpdateTripAsync(int tripId);
+        Task<Result<bool>> DeleteTripAsync(int tripId);
+        Task<Result<bool>> DeleteAllTripsByUserId();
+        Task<Result<Trip>> CreateTripAsync(Trip trip);
+    }
+}
