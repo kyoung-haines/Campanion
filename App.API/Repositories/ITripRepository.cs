c37527a06d05f56a8273de04b002442d338fe5c3
kyoung-haines <k.younghaines@gmail.com>
2026-06-18 12:47:31 -0400

    Commented out two methods that need to be moved

--- App.API/Repositories/ITripRepository.cs
+++ App.API/Repositories/ITripRepository.cs
@@ -5,10 +5,12 @@ namespace App.API.Repositories
     public interface ITripRepository
     {
         Task<Result<List<Trip>>> GetAllTripsAsync();
-        Task<Result<List<Trip>>> GetTripsByUserIdAsync(int userId);
+        //REMOVE - MOVE TO AppUserTrip repository layer when created
+        //Task<Result<List<Trip>>> GetTripsByUserIdAsync(int userId);
         Task<Result<Trip>> UpdateTripAsync(int tripId);
         Task<Result<bool>> DeleteTripAsync(int tripId);
-        Task<Result<bool>> DeleteAllTripsByUserId();
+        //REMOVE - MOVE TO AppUserTrip repository layer when created
+        //Task<Result<bool>> DeleteAllTripsByUserId(int userId);
         Task<Result<Trip>> CreateTripAsync(Trip trip);
     }
 }
