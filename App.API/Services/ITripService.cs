b7e5cad276aeb0d825235a414fa2621967781f22
kyoung-haines <k.younghaines@gmail.com>
2026-06-19 15:04:26 -0400

    Created ITripService Interface for the service layer

---
+++ App.API/Services/ITripService.cs
@@ -0,0 +1,12 @@
+﻿using App.API.Models.Trips;
+
+namespace App.API.Services
+{
+    public interface ITripService
+    {
+        Task<Result<List<Trip>>> GetAllTripsAsync();
+        Task<Result<bool>> DeleteTripAsync(int tripId);
+        Task<Result<Trip>> CreateTripAsync(Trip trip);
+        Task<Result<Trip>> UpdateTripAsync(int tripId);
+    }
+}
