b1621ff61ec4b4b5a7653594406ac313017e55e4
kyoung-haines <k.younghaines@gmail.com>
2026-06-20 19:04:48 -0400

    Added DTO to transfer all member data for a given Trip entity

---
+++ App.API/Dtos/Trips/TripsDtos/TripDto.cs
@@ -0,0 +1,33 @@
+﻿using App.API.Enums;
+using App.API.Models.Identity;
+using App.API.Models.Trips;
+
+namespace App.API.Dtos.Trips.TripsDtos
+{
+    public class TripDto
+    {
+        private readonly int _tripId;
+        private readonly string? _tripName;
+        private readonly string _tripStatus;
+        private readonly string _tripStartDate;
+        private readonly string _tripEndDate;
+        private readonly string _tripCreationDate;
+        private readonly List<string> _tripAttendees = new List<string>();
+
+        public TripDto(Trip trip)
+        {
+            _tripId = trip.TripId;
+            _tripName = trip.TripName;
+            _tripStatus = Convert.ToString(trip.TripStatus);
+            _tripStartDate = Convert.ToString(trip.TripStartDate);
+            _tripEndDate = Convert.ToString(trip.TripEndDate);
+            _tripCreationDate = Convert.ToString(trip.TripCreationDate);
+            
+            foreach (var user in trip.TripAttendees)
+            {
+                var attendeeName = user.AppUserFirstName + " " + user.AppUserLastName;
+                _tripAttendees.Add(attendeeName);
+            }
+        }
+    }
+}
