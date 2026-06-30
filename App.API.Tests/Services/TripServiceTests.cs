d436af4dde04898f3c4d2281b3b6df3ce0727852
kyoung-haines <k.younghaines@gmail.com>
2026-06-30 16:53:44 -0400

    Attempted bug fix

--- App.API.Tests/Services/TripServiceTests.cs
+++ App.API.Tests/Services/TripServiceTests.cs
@@ -131,10 +131,10 @@ namespace App.API.Tests.Services
 
             var expectedResult = Result<bool>.Success(true);
 
-            var actualResult = await _tripService.DeleteTripAsync(1);
+            var actualResult = await _tripService.DeleteTripAsync(1);            
 
-            Assert.IsTrue(expectedResult.Data);
-            Assert.IsTrue(actualResult.Data);
+            Assert.IsTrue(expectedResult.Succeeded);
+            Assert.IsTrue(actualResult.Succeeded);
         }
 
         [TestMethod]
