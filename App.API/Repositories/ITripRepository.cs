dd968f51fa93d366d37d1929eb484aad55de85e5
kyoung-haines <k.younghaines@gmail.com>
2026-07-05 12:20:25 -0400

    Modified method return types

--- App.API/Repositories/ITripRepository.cs
+++ App.API/Repositories/ITripRepository.cs
@@ -4,14 +4,14 @@ namespace App.API.Repositories
 {
     public interface ITripRepository
     {
-        Task<Result<List<Trip>>> GetAllTripsAsync();
+        Task<List<Trip>> GetAllTripsAsync();
         //REMOVE - MOVE TO AppUserTrip repository layer when created
         //Task<Result<List<Trip>>> GetTripsByUserIdAsync(int userId);
-        Task<Result<Trip>> UpdateTripAsync(int tripId);
-        Task<Result<bool>> DeleteTripAsync(int tripId);
+        Task<Trip> UpdateTripAsync(int tripId);
+        Task<bool> DeleteTripAsync(int tripId);
         //REMOVE - MOVE TO AppUserTrip repository layer when created
         //Task<Result<bool>> DeleteAllTripsByUserId(int userId);
-        Task<Result<Trip>> CreateTripAsync(Trip trip);
-        Task<Result<Trip>> GetTripByTripIdAsync(int tripId);
+        Task<Trip> CreateTripAsync(Trip trip);
+        Task<Trip> GetTripByTripIdAsync(int tripId);
     }
 }
