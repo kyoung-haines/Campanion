3f68633071bc1da13f5ccce2dce5e3790adc5748
kyoung-haines <k.younghaines@gmail.com>
2026-07-05 13:45:03 -0400

    Custom exception to throw for non-existent Trips

---
+++ App.API/Exceptions/TripExceptions/TripNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace App.API.Exceptions.TripExceptions
+{
+    public class TripNotFoundException : Exception
+    {
+        public TripNotFoundException() { }
+        public TripNotFoundException(string message) : base(message) { }
+        public TripNotFoundException(string message, Exception innerException) : base(message, innerException) { } 
+    }
+}
