6dbbb153cef1d224d02e89021ccb54f7d26911a0
kyoung-haines <k.younghaines@gmail.com>
2026-07-06 13:27:31 -0400

    Modified return types

--- App.API/Repositories/ICampgroundRepository.cs
+++ App.API/Repositories/ICampgroundRepository.cs
@@ -4,10 +4,10 @@ namespace App.API.Repositories
 {
     public interface ICampgroundRepository
     {
-        public Task<Result<List<Campground>>> GetAllCampgroundsAsync();
-        public Task<Result<Campground>> GetCampgroundByIdAsync(int id);
-        public Task<Result<Campground>> UpdateCampgroundAsync(Campground campground);
-        public Task<Result<bool>> DeleteCampgroundAsync(int id);
-        public Task<Result<Campground>> AddCampgroundAsync(Campground newCampground);
+        public Task<List<Campground>> GetAllCampgroundsAsync();
+        public Task<Campground> GetCampgroundByIdAsync(int id);
+        public Task<Campground> UpdateCampgroundAsync(Campground campground);
+        public Task<bool> DeleteCampgroundAsync(int id);
+        public Task<Campground> AddCampgroundAsync(Campground newCampground);
     }
 }
