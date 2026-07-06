283ee96f79090b884868c036432099f81cd25b1c
kyoung-haines <k.younghaines@gmail.com>
2026-07-06 14:46:46 -0400

    Modified return types

--- App.API/Repositories/IAppUserRepository.cs
+++ App.API/Repositories/IAppUserRepository.cs
@@ -5,12 +5,12 @@ namespace App.API.Repositories
 {
     public interface IAppUserRepository
     {
-        public Task<Result<List<AppUser>>> GetAllAppUsersAsync();
-        public Task<Result<List<AppUser>>> GetAllAdminAppUsersAsync();
-        public Task<Result<List<AppUser>>> GetAllRegularAppUsersAsync();
-        public Task<Result<AppUser>> GetAppUserByIdAsync(int appUserId);
-        public Task<Result<bool>> DeleteAppUserAsync(int appUserId);
-        public Task<Result<AppUser>> UpdateAppUserAsync(int appUserId);
+        public Task<List<AppUser>> GetAllAppUsersAsync();
+        public Task<List<AppUser>> GetAllAdminAppUsersAsync();
+        public Task<List<AppUser>> GetAllRegularAppUsersAsync();
+        public Task<AppUser> GetAppUserByIdAsync(int appUserId);
+        public Task<bool> DeleteAppUserAsync(int appUserId);
+        public Task<AppUser> UpdateAppUserAsync(int appUserId);
         public Task<IdentityResult> CreateAppUserAsync(AppUser newUser, IdentityResult result);
     }
 }
