a4352161caa7af769053d5810278891e0e5a69e4
kyoung-haines <k.younghaines@gmail.com>
2026-07-07 16:27:42 -0400

    Added skeleton API controller

---
+++ App.API/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using App.API.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("login")]
+        public async Task<Profile> UserLogin()
+        {
+
+        }
+    }
+}
