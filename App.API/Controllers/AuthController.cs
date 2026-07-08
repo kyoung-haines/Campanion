30fb32f85de1e20403d051db4f1f4087faf8b21d
kyoung-haines <k.younghaines@gmail.com>
2026-07-08 13:10:05 -0400

    Added login method

--- App.API/Controllers/AuthController.cs
+++ App.API/Controllers/AuthController.cs
@@ -1,5 +1,10 @@
 ﻿using App.API.Models.Identity;
+using App.API.Services;
+using Campanion.Shared.Dtos.AuthDtos;
+using Humanizer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.API.Controllers
@@ -8,10 +13,29 @@ namespace App.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        [HttpGet("login")]
-        public async Task UserLogin()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
+
+        public AuthController(UserManager<AppUser> userManager, ITokenService tokenService)
         {
-            return;
+            _userManager = userManager;
+            _tokenService = tokenService;
+        }
+
+        [AllowAnonymous]
+        [HttpGet("login")]
+        public async Task<ActionResult<AuthResponseDto>> UserLogin(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user is null || !await _userManager.CheckPasswordAsync(user, loginDto.Password))
+            {
+                return Unauthorized();
+            }
+
+            var token = await _tokenService.GenerateTokenAsync(user);
+
+            return Ok(new AuthResponseDto { Token = token });
         }
     }
 }
