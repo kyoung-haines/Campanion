76cb9ca90df6fb4cae35853761505acdafad6cfc
kyoung-haines <k.younghaines@gmail.com>
2026-07-08 16:00:20 -0400

    Added custom exception

---
+++ App.API/Exceptions/AppUserExceptions/InvalidUserEmailException.cs
@@ -0,0 +1,9 @@
+﻿namespace App.API.Exceptions.AppUserExceptions
+{
+    public class InvalidUserEmailException : Exception
+    {
+        public InvalidUserEmailException() { }
+        public InvalidUserEmailException(string message) : base(message) { }
+        public InvalidUserEmailException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
