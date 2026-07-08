5090451a9df8eac991a970f863778b39146bc852
kyoung-haines <k.younghaines@gmail.com>
2026-07-08 16:22:00 -0400

    Working on RegisterUser method

--- App.API/Controllers/AuthController.cs
+++ App.API/Controllers/AuthController.cs
@@ -15,11 +15,13 @@ namespace App.API.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
+        private IAppUserService _userService;
 
-        public AuthController(UserManager<AppUser> userManager, ITokenService tokenService)
+        public AuthController(UserManager<AppUser> userManager, ITokenService tokenService, IAppUserService userService)
         {
             _userManager = userManager;
             _tokenService = tokenService;
+            _userService = userService;
         }
 
         [AllowAnonymous]
@@ -37,5 +39,54 @@ namespace App.API.Controllers
 
             return Ok(new AuthResponseDto { Token = token });
         }
+
+        // take in reg dto -> create profile dto
+        // create profile from create profile dto
+        // serve profile response dto?
+        /*
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<ProfileResponseDto>> RegisterUser(RegistrationDto regDto)
+        {
+            AppUser newUser = new AppUser()
+            {
+                Email = regDto.AppUserEmail,
+                AppUserFirstName = regDto.AppUserFirstName,
+                AppUserLastName = regDto.AppUserLastName,
+                AppUserProvince = regDto.AppUserProvince,
+                AppUserCountry = regDto.AppUserCountry
+            };
+
+            var saveUserResult = await _userService.CreateAppUserAsync(newUser);
+
+            if (saveUserResult.Succeeded == true)
+            {
+                var user = await _userService.GetAppUserByEmailAsync(newUser.Email);
+                var profileDto = new ProfileResponseDto()
+                {
+                    
+                };
+            }
+        }
+        */
+    }
+
+    public class RegistrationDto()
+    {
+        public string AppUserPassword = string.Empty;
+        public string AppUserEmail = string.Empty;
+        public string AppUserPhone = string.Empty;
+        public string AppUserFirstName = string.Empty;
+        public string AppUserLastName = string.Empty;
+        public string AppUserStreetAddress = string.Empty;
+        public string AppUserCity = string.Empty;
+        public string AppUserProvince = string.Empty;
+        public string AppUserCountry = string.Empty;
+        public string AppUserPostalCode = string.Empty;
+    }
+
+    public class ProfileResponseDto()
+    {
+        public string ProfileUsername = string.Empty;
     }
 }
