e0293592810d31ff3f1e0ef88854a2184df18233
kyoung-haines <k.younghaines@gmail.com>
2026-07-09 14:40:48 -0400

    Added RegisterNewUser method

--- App.API/Services/AuthService.cs
+++ App.API/Services/AuthService.cs
@@ -1,5 +1,6 @@
 ﻿using App.API.Models.Identity;
 using Campanion.Shared.Dtos.AuthDtos;
+using Campanion.Shared.Dtos.ProfileDtos;
 
 namespace App.API.Services
 {
@@ -12,64 +13,56 @@ namespace App.API.Services
 
         public async Task<Result<RegisterResponseDto>> RegisterNewUser(RegistrationDto regDto)
         {
-            try
+            _logger.LogInformation("AuthService method called: RegisterNewUser...");
+            _logger.LogInformation("Attempting to register a new user...");
+
+            _logger.LogInformation("Creating a new user...");
+            AppUser newUser = new AppUser
             {
-                _logger.LogInformation("AuthService method called: RegisterNewUser...");
-                _logger.LogInformation("Attempting to register a new user...");
+                UserName = regDto.AppUserUsername,
+                AppUserFirstName = regDto.AppUserFirstName,
+                AppUserLastName = regDto.AppUserLastName,
+                AppUserProvince = regDto.AppUserProvince,
+                AppUserCountry = regDto.AppUserCountry
+            };
 
-                _logger.LogInformation("Creating a new user...");
-                AppUser newUser = new AppUser
-                {
-                    UserName = regDto.AppUserUsername,
-                    AppUserFirstName = regDto.AppUserFirstName,
-                    AppUserLastName = regDto.AppUserLastName,
-                    AppUserProvince = regDto.AppUserProvince,
-                    AppUserCountry = regDto.AppUserCountry
-                };
+            var createUserResult = await _userService.CreateAppUserAsync(newUser, regDto.AppUserPassword);
 
-                var createUserResult = await _userService.CreateAppUserAsync(newUser, regDto.AppUserPassword);
-
-                if (!createUserResult.Succeeded)
-                {
-                    return Result<RegisterResponseDto>.Failure("Failed to register new user.");
-                }
-
-                // this is probably not even neessary
-                //var createProfileDto = new CreateProfileDto
-                //{
-
-                //};
-
-                var newProfileResult = await _profileService.CreateNewProfileAsync(newUser);
-
-                if (!newProfileResult.Succeeded)
-                {
-                    return Result<RegisterResponseDto>.Failure("Failed to register new user.");
-                }
-
-                var profile = newProfileResult.Data;
-                var token = await _tokenService.GenerateTokenAsync(newUser);
-                var profileResponseDto = new ProfileResponseDto
-                {
-                    ProfileId = Convert.ToString(profile.ProfileId),
-                    ProfileUsername = profile.ProfileUsername,
-                    ProfileImagePath = profile.ProfileImagePath,
-                    ProfileCreatedAt = Convert.ToString(profile.ProfileCreatedAt),
-                    AppUserId = Convert.ToString(profile.AppUserId)
-                };
-                var regResponseDto = new RegisterResponseDto
-                {
-                    Token = token,
-                    Profile = profileResponseDto
-                };
-
-                return Result<RegisterResponseDto>.Success(regResponseDto);
-            }
-            catch (Exception ex)
+            if (!createUserResult.Succeeded)
             {
-
-                throw;
+                return Result<RegisterResponseDto>.Failure("Failed to register new user.");
             }
+
+            // this is probably not even neessary
+            //var createProfileDto = new CreateProfileDto
+            //{
+
+            //};
+
+            var newProfileResult = await _profileService.CreateNewProfileAsync(newUser);
+
+            if (!newProfileResult.Succeeded)
+            {
+                return Result<RegisterResponseDto>.Failure("Failed to register new user.");
+            }
+
+            var profile = newProfileResult.Data;
+            var token = await _tokenService.GenerateTokenAsync(newUser);
+            var profileResponseDto = new ProfileResponseDto
+            {
+                ProfileId = Convert.ToString(profile.ProfileId),
+                ProfileUsername = profile.ProfileUsername,
+                ProfileImagePath = profile.ProfileImagePath,
+                ProfileCreatedAt = Convert.ToString(profile.ProfileCreatedAt),
+                AppUserId = Convert.ToString(profile.AppUserId)
+            };
+            var regResponseDto = new RegisterResponseDto
+            {
+                Token = token,
+                Profile = profileResponseDto
+            };
+
+            return Result<RegisterResponseDto>.Success(regResponseDto);
         }
 
         public async Task<Result<bool>> IsValidPassword(string password)
