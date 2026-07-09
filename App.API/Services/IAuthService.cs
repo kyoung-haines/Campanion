77d0523ee202b3b82992a6f3b4b3efa50b6b7a4d
kyoung-haines <k.younghaines@gmail.com>
2026-07-09 11:50:14 -0400

    Added class skeleton

---
+++ App.API/Services/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using App.API.Models.Identity;
+using Campanion.Shared.Dtos.AuthDtos;
+namespace App.API.Services
+{
+    public interface IAuthService
+    {
+        public Task<Profile> CreateUserProfileAsync(RegistrationDto regDto);
+    }
+}
