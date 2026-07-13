865f7592ea3d9aab8cb362e6fc2bc5ac66692fbf
kyoung-haines <k.younghaines@gmail.com>
2026-07-13 15:02:05 -0400

    Modified RegisterUser method

--- App.API/Controllers/AuthController.cs
+++ App.API/Controllers/AuthController.cs
@@ -14,17 +14,19 @@ namespace App.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly ILogger<AuthController> _logger;
         private readonly UserManager<AppUser> _userManager;
         private readonly ITokenService _tokenService;
         private IAppUserService _userService;
         private IAuthService _authService;
 
-        public AuthController(UserManager<AppUser> userManager, ITokenService tokenService, IAppUserService userService, IAuthService authService)
+        public AuthController(UserManager<AppUser> userManager, ITokenService tokenService, IAppUserService userService, IAuthService authService, ILogger<AuthController> logger)
         {
             _userManager = userManager;
             _tokenService = tokenService;
             _userService = userService;
             _authService = authService;
+            _logger = logger;
         }
 
         [AllowAnonymous]
@@ -45,21 +47,37 @@ namespace App.API.Controllers
 
         [AllowAnonymous]
         [HttpPost("register")]
-        public async Task<ActionResult<ProfileResponseDto>> RegisterUser(RegistrationDto registrationDto)
+        public async Task<ActionResult<RegisterResponseDto>> RegisterUser(RegistrationDto registrationDto)
         {
+            _logger.LogInformation("AuthController method called: RegisterUser...");
+            _logger.LogInformation("Contacting: /api/v1/auth/register...");
+
             var regResponseDto = await _authService.RegisterNewUserAsync(registrationDto);
-            var regResponse = regResponseDto.Data;
-            var profileDto = regResponse.Profile;
 
-            var profileResponseDto = new ProfileResponseDto
+            if (regResponseDto.Succeeded != true)
             {
-                ProfileId = Convert.ToString(profileDto.ProfileId),
-                ProfileUsername = profileDto.ProfileUsername,
-                ProfileCreatedAt = Convert.ToString(profileDto.ProfileCreatedAt),
-                ProfileImagePath = profileDto.ProfileImagePath,
-            };
+                _logger.LogWarning("Error creating the RegistrationResponseDto...Process aborted...");
 
-            return Ok(profileResponseDto);
+                return BadRequest(regResponseDto.Error); // returns error message from the Result object
+            }
+
+            var regResponseData = regResponseDto.Data;
+
+            return Created("/api/v1/profile", regResponseData);
         }
+
+        /*
+         * not sure if I want to keep this here or not...
+         * retrieving the user profile isn't exactly an Authorization function but
+         * there is an argument for there being a function to retrieve the user profile directly
+         * at successful user registration time - however, 
+         * the logic will likely overlap largely with a similar method in the AppUserController instead...
+        [Authorize(Roles="Role.Member", "Role.Admin")]
+        [HttpGet("profile")]
+        public async Task<ActionResult><ProfileResponseDto> GetNewUserProfile()
+        {
+
+        }
+        */
     }
 }
