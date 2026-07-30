2293a7f6a9489a36d9ac7d850f2ec0dc6a64bf2d
kyoung-haines <k.younghaines@gmail.com>
2026-07-30 16:30:40 -0400

    Added package reference

--- App.API/Controllers/UsersController.cs
+++ App.API/Controllers/UsersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Campanion.Shared.Dtos.AppUserDtos;
 
 namespace App.API.Controllers
 {
