6c1ed7105a974c3c15988cf76dc106ddfa8a159f
kyoung-haines <k.younghaines@gmail.com>
2026-08-01 12:16:17 -0400

    Added missing properties

--- App.API/Seeders/ProfileSeeder.cs
+++ App.API/Seeders/ProfileSeeder.cs
@@ -33,6 +33,8 @@ namespace App.API.Seeders
                     AppUserId = user.Id,
                     ProfileCreatedAt = DateTime.UtcNow,
                     ProfileImagePath = "https://picsum.photos/seed/picsum/200",
+                    ProfileUserProvince = user.AppUserProvince,
+                    ProfileUserCountry = user.AppUserCountry,
                     ProfileOwner = user,
                     ProfileUsername = user.AppUserFirstName + user.AppUserLastName + rand.NextInt64()
                 };
