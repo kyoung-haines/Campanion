5ba965d66b459741f009cb65d85d3fc17f7287c7
kyoung-haines <k.younghaines@gmail.com>
2026-08-03 16:46:28 -0400

    Modified return types

--- App.API/Services/IProfileService.cs
+++ App.API/Services/IProfileService.cs
@@ -1,5 +1,6 @@
 ﻿using App.API.Models.Campgrounds;
 using App.API.Models.Identity;
+using App.API.Models.Trips;
 using Campanion.Shared.Dtos.ProfileDtos;
 
 namespace App.API.Services
@@ -11,7 +12,7 @@ namespace App.API.Services
         public Task<Result<bool>> DeleteProfileAsync(int id);
         public Task<Result<Profile>> CreateNewProfileAsync(AppUser newUser);
         public Task<Result<ProfileResponseDto>> GetProfileByAppUserIdAsync(AppUser user);
-        public Task<List<AppUserFavouriteCampground>> RetrieveProfileUserFavouriteCampgrounds(AppUserFavouriteCampgroundService appUserFavouriteCampgroundService, AppUser appUser);
-        public Task RetrieveProfileOwnerUpcomingTrips(AppUser appUser);
+        public Task<Result<List<AppUserFavouriteCampground>>> RetrieveProfileUserFavouriteCampgrounds(AppUserFavouriteCampgroundService appUserFavouriteCampgroundService, AppUser appUser);
+        public Task<Result<List<AppUserTrip>>> RetrieveProfileOwnerUpcomingTrips(AppUser appUser);
     }
 }
