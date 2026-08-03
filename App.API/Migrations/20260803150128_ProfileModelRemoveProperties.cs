17b2f65c6600f3962c94565046e75d48760b8022
kyoung-haines <k.younghaines@gmail.com>
2026-08-03 11:33:29 -0400

    Properties modified

---
+++ App.API/Migrations/20260803150128_ProfileModelRemoveProperties.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace App.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProfileModelRemoveProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileUserCountry",
+                table: "Profiles");
+
+            migrationBuilder.DropColumn(
+                name: "ProfileUserProvince",
+                table: "Profiles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileUserCountry",
+                table: "Profiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileUserProvince",
+                table: "Profiles",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
