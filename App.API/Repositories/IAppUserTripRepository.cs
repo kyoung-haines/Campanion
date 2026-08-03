2763e978f19d0218817cebe339568d4f0e422825
kyoung-haines <k.younghaines@gmail.com>
2026-08-03 16:39:58 -0400

    Modified method return type

---
+++ App.API/Repositories/IAppUserTripRepository.cs
@@ -0,0 +1,11 @@
+﻿using App.API.Models.Identity;
+using App.API.Models.Trips;
+
+namespace App.API.Repositories
+{
+    public interface IAppUserTripRepository
+    {
+        public Task<AppUserTrip> AddNewTripAsync(AppUser appUser, Trip trip);
+        public Task<IEnumerable<AppUserTrip>> RetrieveAllTripsByUserIdAsync(AppUser appUser, Trip trip);
+    }
+}
