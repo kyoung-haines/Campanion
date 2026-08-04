d565da42c3f48927b2a9acc95ecb0db1ee94189e
kyoung-haines <k.younghaines@gmail.com>
2026-08-04 12:18:26 -0400

    Added helper method to convert object into DTO

--- App.API/Models/Trips/Trip.cs
+++ App.API/Models/Trips/Trip.cs
@@ -1,4 +1,5 @@
-﻿using App.API.Enums;
+﻿using Campanion.Shared.Dtos.TripDtos;
+using App.API.Enums;
 using App.API.Models.Identity;
 namespace App.API.Models.Trips
 {
@@ -11,5 +12,28 @@ namespace App.API.Models.Trips
         public DateTime TripEndDate { get; set; }
         public DateTime TripCreationDate { get; set; }
         public List<AppUser> TripAttendees { get; set; } = new List<AppUser>();
-    }
+
+        // HELPERS
+        public async Task<TripDto> ConvertTripObjecToTripDtoAsync(Trip trip)
+        {
+            var tripDto = new TripDto
+            {
+                TripId = Convert.ToString(trip.TripId),
+                TripName = trip.TripName,
+                TripStatus = Convert.ToString(trip.TripStatus),
+                TripStartDate = trip.TripStartDate.ToString(),
+                TripEndDate = trip.TripEndDate.ToString(),
+                TripCreationDate = trip.TripCreationDate.ToString(),
+                TripAttendees = null
+            };
+
+            foreach (var attendee in trip.TripAttendees)
+            {
+                var tripAttendee = attendee.AppUserFirstName + attendee.AppUserLastName;
+                tripDto.TripAttendees.Add(tripAttendee);
+            }
+
+            return tripDto;
+        }
+    }    
 }
