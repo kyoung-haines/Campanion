1faa7baa75d73afb733497d5ecfe3b97f154c6fe
kyoung-haines <k.younghaines@gmail.com>
2026-08-04 12:20:58 -0400

    Added missing logic and constructor

--- App.API/Services/AppUserTripService.cs
+++ App.API/Services/AppUserTripService.cs
@@ -1,4 +1,5 @@
-﻿using App.API.Models.Identity;
+﻿using Campanion.Shared.Dtos.TripDtos;
+using App.API.Models.Identity;
 using App.API.Models.Trips;
 using App.API.Repositories;
 
@@ -7,14 +8,29 @@ namespace App.API.Services
     public class AppUserTripService : IAppUserTripService
     {
         private readonly IAppUserTripRepository _tripRepository;
-        private readonly ILogger<AppUserTripService> _logger;
-
-        public async Task<Result<AppUserTrip>> AddNewAppUserTripAsync(AppUser appUser, Trip trip)
+        private readonly ILogger<IAppUserTripService> _logger;
+        private readonly IAppUserService _appUserService;
+        private readonly ITripService _tripService;
+        public AppUserTripService(IAppUserTripRepository tripRepo, ILogger<IAppUserTripService> logger, IAppUserService appUserService, ITripService tripService)
         {
+            _tripRepository = tripRepo;
+            _logger = logger;
+            _appUserService = appUserService;
+            _tripService = tripService;
+        }
+
+        public async Task<Result<AppUserTrip>> AddNewAppUserTripAsync(string appUserId, int tripId)
+        {
+            TripDto trip = null;
+            AppUser appUser = null;
+
             try
             {
                 _logger.LogInformation("AppUserTripService method called: AddNewTripAsync...");
                 _logger.LogInformation($"Attempting to add a new app user trip to the database for user: {appUser.Id} and trip: {trip.TripId}...");
+
+                var tripResult = await _tripService.GetTripByIdAsync(tripId);
+                trip = tripResult.Data;
 
                 var appUserTrip = await _tripRepository.AddNewAppUserTripAsync(appUser, trip);
 
@@ -28,13 +44,19 @@ namespace App.API.Services
            
         }
 
-        public async Task<Result<IEnumerable<AppUserTrip>>> RetrieveAllAppUserTripsByUserIdAsync(AppUser appUser)
+        public async Task<Result<IEnumerable<AppUserTrip>>> RetrieveAllAppUserTripsByUserIdAsync(string appUserId)
         {
-            _logger.LogInformation("AppUserTripService method called: RetrieveAllAppUserTripsByUserIdAsync...");
-            _logger.LogInformation($"Attempting to retrieve all trips for user: {appUser.Id}...");
-
+            AppUser appUser = null;
+            
             try
             {
+                _logger.LogInformation("AppUserTripService method called: RetrieveAllAppUserTripsByUserIdAsync...");
+                
+                var appUserResult = await _appUserService.GetAppUserByIdAsync(appUserId);
+                appUser = appUserResult.Data;
+
+                _logger.LogInformation($"Attempting to retrieve all trips for user: {appUser.Id}...");
+
                 var appUserTrips = await _tripRepository.RetrieveAllAppUserTripsByUserIdAsync(appUser);
 
                 return Result<IEnumerable<AppUserTrip>>.Success(appUserTrips);
