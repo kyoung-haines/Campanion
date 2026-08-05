636a6921b5c84b5c8988a56f4cff55206a4c39ff
kyoung-haines <k.younghaines@gmail.com>
2026-08-05 13:55:46 -0400

    Modified exception name

---
+++ App.API/Exceptions/AppUserFavouriteCampgroundExceptions/InvalidAppUserFavouriteCampgroundReferenceException.cs
@@ -0,0 +1,11 @@
+﻿namespace App.API.Exceptions.AppUserFavouriteCampgroundExceptions
+{
+    public class InvalidAppUserFavouriteCampgroundReferenceException : NullReferenceException
+    {
+        public InvalidAppUserFavouriteCampgroundReferenceException() { }
+
+        public InvalidAppUserFavouriteCampgroundReferenceException(string message  = "Invalid favourite campground object. The reference is null.") : base(message) { }
+
+        public InvalidAppUserFavouriteCampgroundReferenceException(string message = "Invalid favourite campground object. The reference is null.", Exception innerException) : base(message, innerException) { }
+    }
+}
