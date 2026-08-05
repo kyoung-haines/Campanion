55ec0149f113706652e0ceb3cdb6b3d898833dfc
kyoung-haines <k.younghaines@gmail.com>
2026-08-05 13:28:09 -0400

    Added default exception message

--- App.API/Exceptions/CampgroundExceptions/InvalidCampgroundIdException.cs
+++ App.API/Exceptions/CampgroundExceptions/InvalidCampgroundIdException.cs
@@ -3,7 +3,7 @@
     public class InvalidCampgroundIdException : CampgroundException
     {
         public InvalidCampgroundIdException() { }
-        public InvalidCampgroundIdException(string message) : base(message) { }
-        public InvalidCampgroundIdException(string message, Exception innerException) : base(message, innerException) { } 
+        public InvalidCampgroundIdException(string message = "Campground not found. Invalid CampgroundId value.") : base(message) { }
+        public InvalidCampgroundIdException(string message = "Campground not found. Invalid CampgroundId value.", Exception innerException) : base(message, innerException) { } 
     }
 }
