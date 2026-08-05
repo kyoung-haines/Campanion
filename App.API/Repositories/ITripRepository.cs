54dc14750556036c18ad2deb49178ab031286ac8
kyoung-haines <k.younghaines@gmail.com>
2026-08-05 12:53:17 -0400

    Modified method parameter data type

--- App.API/Repositories/ITripRepository.cs
+++ App.API/Repositories/ITripRepository.cs
@@ -5,12 +5,8 @@ namespace App.API.Repositories
     public interface ITripRepository
     {
         Task<List<Trip>> GetAllTripsAsync();
-        //REMOVE - MOVE TO AppUserTrip repository layer when created
-        //Task<Result<List<Trip>>> GetTripsByUserIdAsync(int userId);
-        Task<Trip> UpdateTripAsync(int tripId);
+        Task<Trip> UpdateTripAsync(Trip trip);
         Task<bool> DeleteTripAsync(int tripId);
-        //REMOVE - MOVE TO AppUserTrip repository layer when created
-        //Task<Result<bool>> DeleteAllTripsByUserId(int userId);
         Task<Trip> CreateTripAsync(Trip trip);
         Task<Trip> GetTripByTripIdAsync(int tripId);
     }
