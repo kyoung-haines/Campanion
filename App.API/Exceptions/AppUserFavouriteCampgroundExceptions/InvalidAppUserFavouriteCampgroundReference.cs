3fd5359112df9161a15cbf03e43d587d6c72943e
kyoung-haines <k.younghaines@gmail.com>
2026-08-05 13:53:07 -0400

    Added custom exception for null references

---
+++ App.API/Exceptions/AppUserFavouriteCampgroundExceptions/InvalidAppUserFavouriteCampgroundReference.cs
@@ -0,0 +1,11 @@
+﻿namespace App.API.Exceptions.AppUserFavouriteCampgroundExceptions
+{
+    public class InvalidAppUserFavouriteCampgroundReference : NullReferenceException
+    {
+        public InvalidAppUserFavouriteCampgroundReference() { }
+
+        public InvalidAppUserFavouriteCampgroundReference(string message  = "Invalid favourite campground object. The reference is null.") : base(message) { }
+
+        public InvalidAppUserFavouriteCampgroundReference(string message = "Invalid favourite campground object. The reference is null.", Exception innerException) : base(message, innerException) { }
+    }
+}
