0a45429dad39b44fd50f332495b6f00fef0a2302
kyoung-haines <k.younghaines@gmail.com>
2026-08-06 13:43:00 -0400

    Modified method name

--- App.API/Repositories/IAppUserFavouriteCampgroundRepository.cs
+++ App.API/Repositories/IAppUserFavouriteCampgroundRepository.cs
@@ -9,6 +9,6 @@ namespace App.API.Repositories
         Task<AppUserFavouriteCampground> UpdateFavouriteCampgroundAsync(AppUserFavouriteCampground favouriteCampground);
         Task<AppUserFavouriteCampground> GetFavouriteCampgroundByCampgroundId(int campgroundId);
         Task<AppUserFavouriteCampground> AddNewAppUserFavouriteCampgroundAsync(AppUserFavouriteCampground appUserFavouriteCampground);
-        Task<List<AppUserFavouriteCampground>> GetAllAppUserFavouriteCampgrounds();
+        Task<List<AppUserFavouriteCampground>> GetAllAppUsersFavouriteCampgrounds();
     }
 }
--- App.API/Services/IAppUserFavouriteCampgroundService.cs
+++ App.API/Services/IAppUserFavouriteCampgroundService.cs
@@ -7,7 +7,7 @@ namespace App.API.Services
         public Task<Result<AppUserFavouriteCampgroundDto>> AddFavouriteCampgroundAsync(AppUserFavouriteCampgroundDto favCampgroundDto);
         public Task<Result<AppUserFavouriteCampgroundsDto>> GetAllFavouriteCampgroundsByUserIdAsync(string appUserId);
         public Task<Result<AppUserFavouriteCampgroundDto>> GetFavouriteCampgroundByPrimaryKey(int campId, string userId);
-        public Task<Result<List<AppUserFavouriteCampgroundDto>>> GetAllAppUserFavouriteCampgroundDtos();
+        public Task<Result<List<AppUserFavouriteCampgroundDto>>> GetAllAppUsersFavouriteCampgroundDtos();
 
     }
 }
