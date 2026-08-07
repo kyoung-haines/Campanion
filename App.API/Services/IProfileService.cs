0437d1c55f63efe2c20b74df1db3f6909934e5b4
kyoung-haines <k.younghaines@gmail.com>
2026-08-07 14:52:44 -0400

    Fixed method signatures

--- App.API/Services/IProfileService.cs
+++ App.API/Services/IProfileService.cs
@@ -2,17 +2,18 @@
 using App.API.Models.Identity;
 using App.API.Models.Trips;
 using Campanion.Shared.Dtos.ProfileDtos;
+using Campanion.Shared.Dtos.AppUserDtos;
 
 namespace App.API.Services
 {
     public interface IProfileService
     {
         public Task<Result<Profile>> GetProfileByProfileIdAsync(int id);
-        public Task<Result<Profile>> UpdateProfileAsync(int id);
+        public Task<Result<Profile>> UpdateProfileAsync(ProfileResponseDto profileDto);
         public Task<Result<bool>> DeleteProfileAsync(int id);
         public Task<Result<Profile>> CreateNewProfileAsync(AppUser newUser);
         public Task<Result<ProfileResponseDto>> GetProfileByAppUserIdAsync(AppUser user);
-        public Task<Result<List<AppUserFavouriteCampground>>> RetrieveProfileUserFavouriteCampgrounds(AppUserFavouriteCampgroundService appUserFavouriteCampgroundService, AppUser appUser);
-        public Task<Result<List<AppUserTrip>>> RetrieveProfileOwnerUpcomingTrips(AppUser appUser);
+        public Task<Result<List<AppUserFavouriteCampgroundDto>>> RetrieveProfileUserFavouriteCampgrounds(AppUserFavouriteCampgroundService appUserFavouriteCampgroundService);
+        public Task<Result<List<AppUserTripDto>>> RetrieveProfileOwnerUpcomingTrips(AppUser appUser);
     }
 }
