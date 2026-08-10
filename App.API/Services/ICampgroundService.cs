7eafd4219ec453843040970738479347d5809da3
kyoung-haines <k.younghaines@gmail.com>
2026-08-10 14:10:09 -0400

    Modified return and parameter types

--- App.API/Services/ICampgroundService.cs
+++ App.API/Services/ICampgroundService.cs
@@ -1,16 +1,16 @@
-﻿using App.API.Models.Campgrounds;
+﻿using Campanion.Shared.Dtos.CampgroundDtos;
 
 namespace App.API.Services
 {
     public interface ICampgroundService
     {
         public Task<Result<bool>> DeleteCampgroundAsync(int id);
-        public Task<Result<List<Campground>>> GetAllCampgroundsAsync();
-        public Task<Result<Campground>> GetCampgroundByIdAsync(int id);
-        public Task<Result<Campground>> UpdateCampgroundAsync(Campground originalCampground);
-        public Task<Result<Campground>> AddCampgroundAsync(Campground campground);
+        public Task<Result<List<CampgroundDto>>> GetAllCampgroundsAsync();
+        public Task<Result<CampgroundDto>> GetCampgroundByIdAsync(int id);
+        public Task<Result<CampgroundDto>> UpdateCampgroundAsync(CampgroundDto originalCampgroundDto);
+        public Task<Result<CampgroundDto>> AddCampgroundAsync(CampgroundDto campgroundDto);
         public Task<Result<bool>> CampgroundIdIsExistsAsync(int newCampId);
-        public Task<Result<bool>> IsCampgroundUpdatedAsync(Campground originalCampground, Campground updatedCampground);
+        public Task<Result<bool>> IsCampgroundUpdatedAsync(CampgroundDto originalCampgroundDto, CampgroundDto updatedCampgroundDto);
         public Task<Result<bool>> IsCampgroundAddedAsync(int newCampgroundId);
     }
 }
