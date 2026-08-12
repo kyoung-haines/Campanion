08f18232c7eb0bcf6272f898a04d92684886f370
kyoung-haines <k.younghaines@gmail.com>
2026-08-12 16:51:12 -0400

    Finished current refactor

--- App.API/Models/Campgrounds/Campground.cs
+++ App.API/Models/Campgrounds/Campground.cs
@@ -7,24 +7,24 @@ namespace App.API.Models.Campgrounds
     {
         // TODO: DOC COMMENTS AGAIN
         public int CampgroundId { get; set; }
-        public required string CampgroundName= string.Empty;
-        public string CampgroundImagePath= "images/campgrounds/placeholder-campground.png";
-        public required string CampgroundStreetName= string.Empty;
-        public required string CampgroundCity= string.Empty;
-        public required string CampgroundProvince= string.Empty;
-        public required string CampgroundCountry= string.Empty;
-        public required string CampgroundPostalCode= string.Empty;
-        public required string CampgroundPhone= string.Empty;
-        public required string CampgroundEmail= "N/A";
-        public required CampgroundType CampgroundType= new();
-        public required bool CampgroundIsOpenYearRound= new();
-        public DateOnly? CampgroundOpenDate= new();
-        public DateOnly? CampgroundCloseDate= new();
+        public required string CampgroundName { get; set; } = "";
+        public string CampgroundImagePath { get; set; } = "images/campgrounds/placeholder-campground.png";
+        public required string CampgroundStreetName { get; set; } = string.Empty;
+        public required string CampgroundCity { get; set; } = string.Empty;
+        public required string CampgroundProvince { get; set; } = string.Empty;
+        public required string CampgroundCountry { get; set; } = string.Empty;
+        public required string CampgroundPostalCode { get; set; } = string.Empty;
+        public required string CampgroundPhone { get; set; } = string.Empty;
+        public required string CampgroundEmail { get; set; } = "N/A";
+        public required CampgroundType CampgroundType { get; set; } = new();
+        public required bool CampgroundIsOpenYearRound { get; set; } = new();
+        public DateOnly? CampgroundOpenDate { get; set; } = new();
+        public DateOnly? CampgroundCloseDate { get; set; } = new();
         public required bool CampgroundHasFacilities { get; set; }
-        public List<string>? CampgroundFacilities= new();
-        public required bool CampgroundHasActivities= new();
-        public List<string>? CampgroundActivities= new();
-        public string? CampgroundUrl= string.Empty;
+        public List<string>? CampgroundFacilities { get; set; } = new();
+        public required bool CampgroundHasActivities { get; set; } = new();
+        public List<string>? CampgroundActivities { get; set; } = new();
+        public string? CampgroundUrl { get; set; } = string.Empty;
 
         // Navigational
         public List<AppUserFavouriteCampground> FavouritedBy= new();
