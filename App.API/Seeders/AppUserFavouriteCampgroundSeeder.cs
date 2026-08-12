c91e0c9f33086a713253188e2cb9509f9ade3e24
kyoung-haines <k.younghaines@gmail.com>
2026-08-12 16:51:45 -0400

    Finished logic for seeder method

--- App.API/Seeders/AppUserFavouriteCampgroundSeeder.cs
+++ App.API/Seeders/AppUserFavouriteCampgroundSeeder.cs
@@ -7,7 +7,7 @@ namespace App.API.Seeders
 {
     public static class AppUserFavouriteCampgroundSeeder
     {
-        public static async Task SeedAppUserFavouriteCampgroundsAsync(CampanionDbContext context, IAppUserFavouriteCampgroundService appUserFavouriteCampgroundService, ILogger logger, IAppUserService userService, ICampgroundService campgroundService)
+        public static async Task SeedAppUserFavouriteCampgroundsAsync(CampanionDbContext context, ILogger logger, IAppUserService userService, ICampgroundService campgroundService)
         {
             logger.LogInformation("Attempting to seed AppUserFavouriteCampgrounds...");
 
@@ -17,40 +17,65 @@ namespace App.API.Seeders
             var testAppUsers = testAppUsersResult.Data;
             var testCampgrounds = testCampgroundsResult.Data;
 
-            var testAppUserFavouriteCampground1 = new AppUserFavouriteCampground();
+            var testAppUser1 = testAppUsers[0];
+            var testAppUser2 = testAppUsers[1];
 
-            var testAppUserFavouriteCampground2 = new AppUserFavouriteCampground();
+            var testCampground1 = testCampgrounds[0];
+            var testCampground2 = testCampgrounds[1];
+
+            var testAppUserFavouriteCampground1 = new AppUserFavouriteCampground
+            {
+                AppUserId = testAppUsers[0].AppUserId,
+                CampgroundId = Convert.ToInt32(testCampgrounds[0].CampgroundIdDto),
+                FavouritedAt = DateTime.Now
+            };
+            var testAppUserFavouriteCampground2 = new AppUserFavouriteCampground
+            {
+                AppUserId = testAppUsers[1].AppUserId,
+                CampgroundId = Convert.ToInt32(testCampgrounds[1].CampgroundIdDto),
+                FavouritedAt = DateTime.Now
+            };
 
             int numCampgrounds = testCampgrounds.Count();
             int counter = 0;
 
-            while (counter < numCampgrounds)
+            //while (counter < numCampgrounds)
+            //{
+            //    var campId = testCampgrounds[counter].CampgroundIdDto;
+            //    var appUserId = testAppUsers[counter].AppUserId;
+            //    var favouritedAt = DateTime.Now;
+
+            //    var appUserFavouriteCampground = new AppUserFavouriteCampground
+            //    {
+            //        AppUserId = appUserId.ToString(),
+            //        CampgroundId = Convert.ToInt32(campId),
+            //        FavouritedAt = favouritedAt
+            //    };    
+            //}
+
+            if (!context.AppUserFavouriteCampgrounds.Contains(testAppUserFavouriteCampground1))
             {
-                var campId = testCampgrounds[counter].CampgroundIdDto;
-                var appUserId = testAppUsers[counter].AppUserId;
-                var favouritedAt = DateTime.Now;
+                logger.LogInformation($"Seeding AppUserFavouriteCampground - AppUserId: {testAppUserFavouriteCampground1.AppUserId} | Campground Id: {testAppUserFavouriteCampground1.CampgroundId}");
 
-                var appUserFavouriteCampground = new AppUserFavouriteCampground
-                {
-                    AppUserId = appUserId.ToString(),
-                    CampgroundId = Convert.ToInt32(campId),
-                    FavouritedAt = favouritedAt
-                };
+                context.AppUserFavouriteCampgrounds.Add(testAppUserFavouriteCampground1);
+                await context.SaveChangesAsync();
 
-                if (!context.AppUserFavouriteCampgrounds.Contains(appUserFavouriteCampground))
-                {
-                    logger.LogInformation($"Seeding AppUserFavouriteCampground - AppUserId: {appUserFavouriteCampground.AppUserId} | Campground Id: {appUserFavouriteCampground.CampgroundId}");
-                    
-                    context.AppUserFavouriteCampgrounds.Add(appUserFavouriteCampground);
-                    await context.SaveChangesAsync();
+                counter++;
+            }
 
-                    counter++;
-                }
-                else
-                {
-                    logger.LogInformation("AppUserFavourite record already exists. Skipping...");
-                    return;
-                }
+            if (!context.AppUserFavouriteCampgrounds.Contains(testAppUserFavouriteCampground2))
+            {
+                logger.LogInformation($"Seeding AppUserFavouriteCampground - AppUserId: {testAppUserFavouriteCampground2.AppUserId} | Campground Id: {testAppUserFavouriteCampground2.CampgroundId}");
+
+                context.AppUserFavouriteCampgrounds.Add(testAppUserFavouriteCampground2);
+                await context.SaveChangesAsync();
+
+                counter++;
+            }
+            else
+            {
+                logger.LogInformation("AppUserFavourite record already exists. Skipping...");
+                return;
             }
         }
     }
